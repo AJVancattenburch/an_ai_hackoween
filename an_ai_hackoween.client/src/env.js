095d52252e629eee17c6003762b55be5382a53af
Anthony Vancattenburch <wanderwithme1988@gmail.com>
2023-10-28 14:42:21 -0600

    scrollbar styling if needed in scss variables file

--- an_ai_hackoween.client/src/env.js
+++ an_ai_hackoween.client/src/env.js
@@ -4,3 +4,4 @@ export const useSockets = false
 export const domain = 'dev-82py0l5jt61zn10o.us.auth0.com'
 export const clientId = 'hykoUyNDCrsovYQXXUKObg3y1Iw4UhUS'
 export const audience = 'http://sandbox.com'
+export const apiKey = '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'

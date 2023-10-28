21209a5d21e8fd124ca8105bff386b846194c5ec
Chantha <93282397+ChanthaKammer@users.noreply.github.com>
2023-10-28 14:36:57 -0600

    Image Generation Works

--- an_ai_hackoween.client/src/AppState.js
+++ an_ai_hackoween.client/src/AppState.js
@@ -5,6 +5,7 @@ export const AppState = reactive({
   user: {},
   /** @type {import('./models/Account.js').Account} */
   account: {},
+<<<<<<< Updated upstream
   /** @type {import('./models/Room.js').Room} */
   activeRoom: {},
   rooms: [
@@ -31,4 +32,8 @@ export const AppState = reactive({
       steps: 2
     }
   ]
+=======
+  image: {
+  }
+>>>>>>> Stashed changes
 })
---
+++ an_ai_hackoween.client/src/pages/ImageGenerationPage.vue
@@ -0,0 +1,59 @@
+<template>
+    <section class="container-fluid m-3">
+        <div class="row justify-content-center">
+            <div class="col-md-4 bg-dark p-3 rounded-3">
+                <div class="input-group input-group-sm mb-3">
+                    <span class="input-group-text" id="inputGroup-sizing-sm">Prompt</span>
+                    <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" v-model="editable.prompt">
+                    <button class="btn btn-outline-success" type="button" id="button-addon1" @click="generateImage">Button</button>
+                </div>
+                <div class="bg-light rounded-3 w-100 h-auto" id="imageFrame" style="">
+                    <!-- <h1 class="text-center">Image Placeholder</h1> -->
+                    <img :src="image.src" class="img-fluid object-fit-cover rounded-3">
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+  
+<script>
+  import { computed, onMounted, ref, watchEffect } from 'vue'
+  import { AppState } from '../AppState'
+  import Pop from "../utils/Pop.js"
+  import { logger } from "../utils/Logger.js"  
+  import { imageservice } from '../services/ImageService'
+  
+  export default {
+    components: {
+
+    },
+    setup() {
+      const editable = ref({})
+      let image = ref({})
+      watchEffect(() => {
+        image = AppState.image
+      })
+      onMounted(() => {
+
+      })
+      async function generateImage(){
+        try{
+            const prompt = editable.value.prompt
+            logger.log("Generating prompt:", prompt)
+            await imageservice.generateImage(prompt)
+        } catch (e){
+            Pop.error(e)
+        }
+      }
+      return {
+        editable,
+        image,
+        generateImage
+      } 
+    }
+  }
+  </script>
+  
+  <style scoped lang="scss">
+  
+  </style>

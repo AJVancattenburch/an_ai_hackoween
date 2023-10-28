492c40926db26db033e8cdb492d39dd2a201ff82
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2023-10-28 14:31:28 -0600

    Rooms in about page

---
+++ an_ai_hackoween.client/src/models/Room.js
@@ -0,0 +1,13 @@
+export class Room {
+  constructor(data) {
+    this.id = data.id
+    this.name = data.name
+    this.e = data.e
+    this.location = data.location
+    this.npc = data.npc
+    this.ooi = data.ooi
+    this.kind = data.kind
+    this.description = data.description
+    this.steps = data.steps
+  }
+}

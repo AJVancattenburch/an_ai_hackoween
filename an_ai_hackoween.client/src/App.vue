6610fc769c31eae0977a230f65c0d2656211a17c
Becca <becca.vandewater@gmail.com>
2023-10-28 13:56:09 -0600

    style in working state

--- an_ai_hackoween.client/src/App.vue
+++ an_ai_hackoween.client/src/App.vue
@@ -1,19 +1,14 @@
 <template>
   <header>
-    <Navbar />
   </header>
   <main>
     <router-view />
   </main>
-   <footer class="bg-dark text-light">
-    Made with 💖 by CodeWorks
-  </footer>
 </template>
 
 <script>
 import { computed } from 'vue'
 import { AppState } from './AppState'
-import Navbar from './components/Navbar.vue'
 
 export default {
   setup() {
@@ -21,7 +16,6 @@ export default {
       appState: computed(() => AppState)
     }
   },
-  components: { Navbar }
 }
 </script>
 <style lang="scss">
@@ -31,10 +25,9 @@ export default {
   --main-height: calc(100vh - 32px - 64px);
 }
 
-
-footer {
-  display: grid;
-  place-content: center;
-  height: 32px;
+main{
+  background-image: url(../src/assets/img/grimtolTownWideLowContrast.png);
+  background-size: cover;
+  background-position: top;
 }
 </style>

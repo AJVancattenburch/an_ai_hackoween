49d14db41c41275b8936c2049eb98bc1e63f4998
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2023-10-28 19:50:39 -0600

    rooms is on page

--- an_ai_hackoween.client/src/pages/HomePage.vue
+++ an_ai_hackoween.client/src/pages/HomePage.vue
@@ -12,7 +12,8 @@
     Off Canvas Button
   </button> -->
   <div class="d-flex justify-content-center">
-    <TextBox :text="text" :options="options" />
+    <Rooms />
+    <!-- <TextBox :text="text" :options="options" /> -->
   </div>
   <div class="d-flex justify-content-center align-items-center m-1 mb-4 text-white">
     <p class="m-0 fs-2">

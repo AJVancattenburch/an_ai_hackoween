c89e0d903ed6dc1c176bcbe06d8a13c015036827
Becca <becca.vandewater@gmail.com>
2023-10-28 19:30:20 -0600

    puzzle image fixed

--- an_ai_hackoween.client/src/components/Puzzle.vue
+++ an_ai_hackoween.client/src/components/Puzzle.vue
@@ -1,17 +1,17 @@
 <template>
   <section class="container-fluid bg-black green-border green-color box-size my-5">
     <div class="bg-black p-2">
-      <div class="row justify-content-center">
-        <img class="img-fluid object-fit-cover rounded-3 mb-3" :src="puzzle.image">
+      <div class="row justify-content-center p-3">
+        <img class="img-fluid object-fit-cover green-border generated-img" :src="puzzle.image" v-if="puzzle.image">
       </div>
-      <button class="btn btn-success" @click="generateImage(puzzle.description)">Generate Image</button>
+      <button class="btn green-border text-white mb-3" @click="generateImage(puzzle.description)">Generate Image</button>
       <p>{{ puzzle.description }}</p>
-      <h4>{{ puzzle.example }}</h4>
-      <h4>{{ puzzle.question }}</h4>
+      <p>Example: {{ puzzle.example }}</p>
+      <h4 class="mb-3">{{ puzzle.question }}</h4>
       <div class="input-group input-group-sm mb-3">
-          <span class="input-group-text" id="inputGroup-sizing-sm">Answer</span>
-          <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" v-model="input">
-          <button class="btn btn-success" type="button" id="button-addon1" @click="checkAnswer()">Check</button>
+        <span class="input-group-text" id="inputGroup-sizing-sm">Answer</span>
+        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" v-model="input">
+        <button class="btn btn-success" type="button" id="button-addon1" @click="checkAnswer()">Check</button>
       </div>
       <div>
         <p class="text-danger" v-if="puzzle.correct == false">
@@ -23,30 +23,6 @@
       </div>
     </div>
   </section>
-    <section class="container-fluid bg-black green-border green-color box-size my-5">
-        <div class="bg-black p-2">
-            <div class="row justify-content-center p-3">
-                <img class="img-fluid object-fit-cover green-border" :src="puzzle.image" v-if="puzzle.image">
-            </div>
-            <button class="btn green-border text-white mb-3" @click="generateImage(puzzle.description)">Generate Image</button>
-            <p>{{ puzzle.description }}</p>
-            <p>Example: {{ puzzle.example }}</p>
-            <h4 class="mb-3">{{ puzzle.question }}</h4>
-            <div class="input-group input-group-sm mb-3">
-                <span class="input-group-text" id="inputGroup-sizing-sm">Answer</span>
-                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" v-model="input">
-                <button class="btn btn-success" type="button" id="button-addon1" @click="checkAnswer()">Check</button>
-            </div>
-            <div>
-              <p class="text-danger" v-if="puzzle.correct == false">
-                Your answer is not correct!
-              </p>
-              <p class="green-color" v-if="puzzle.correct">
-                Great job!
-              </p>
-            </div>
-        </div>
-    </section>
 </template>
   
 <script>
@@ -113,6 +89,11 @@
     .box-size{
     width: 90vw;
     height: fit-content;
+    }
+
+    .generated-img{
+      width: 50%;
+      height: 50%;
     }
 
   </style>
